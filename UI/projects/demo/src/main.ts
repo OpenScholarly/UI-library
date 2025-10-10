ea3af71cf0f7d373e12d712e5eeb6b9c17ec7bbd
Wubpooz <mathieu.waharte@gmail.com>
2025-10-10 14:22:20 +0200

    fixed tsconfig + changed directory of theme selector

--- UI/projects/demo/src/main.ts
+++ UI/projects/demo/src/main.ts
@@ -1,5 +1,4 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { inject } from '@angular/core';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 import { TypographyService } from 'ui-components';
